de0df3b3a240d832427f5e9635e83dbca40a1b95
hsk-kr <hsk.coder@gmail.com>
2025-01-28 18:21:57 +0000

    fix live_grep_clipboard require path

--- plugin/licovim.lua
+++ plugin/licovim.lua
@@ -1,4 +1,4 @@
-local live_grep_clipboard = require("custom_plugins.licovim.lua.live_grep_clipboard")
+local live_grep_clipboard = require("licovim.lua.live_grep_clipboard")
 
 vim.api.nvim_create_user_command("LicovimLiveGrepWithPath", function()
 	live_grep_clipboard.open_live_grep_with_path()

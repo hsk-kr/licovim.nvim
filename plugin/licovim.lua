2e79a49d7aed7d87c33d5e44e4e60d286491500e
hsk-kr <hsk.coder@gmail.com>
2025-04-22 12:13:07 +0100

    update testrunner commands

--- plugin/licovim.lua
+++ plugin/licovim.lua
@@ -9,6 +9,18 @@ vim.api.nvim_create_user_command("LicovimLiveGrepWithCurrentPath", function()
 	live_grep.open_live_grep_with_current_path()
 end, {})
 
-vim.api.nvim_create_user_command("LicovimUpscopeTestCurrentFile", function()
+vim.api.nvim_create_user_command("LicovimTestRunnerRun", function()
 	upscope.upscope_test_current_file()
+end, {})
+
+vim.api.nvim_create_user_command("LicovimTestRunnerToggle", function()
+	upscope.toggle_sidebar()
+end, {})
+
+vim.api.nvim_create_user_command("LicovimTestRunnerClean", function()
+	upscope.close_output()
+end, {})
+
+vim.api.nvim_create_user_command("LicovimTestRunnerClose", function()
+	upscope.close_sidebar()
 end, {})
